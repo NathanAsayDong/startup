af4e880a742d27f0f51f686dba58eb0fd0f4dedc
NathanAsayDong <nathan.dong@enzy.co>
2023-10-28 19:16:58 -0600

    made it so filters sort assignments

--- home.js
+++ home.js
@@ -2,7 +2,31 @@ function NateTestButton() {
     console.log("NateTestButton");
 }
 
-// just for testing and demo purposes
+// these variables will use getter functions that pull the list from the database
+// classes = getClasses()
+// assignments = getAssignments();
+
+function getCurrentTime() {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    return `${hours}:${minutes}:${seconds}`;
+}
+
+function displayCurrentTime() {
+    const currentTime = getCurrentTime();
+    const timeDisplay = document.getElementById('timeDisplay');
+    timeDisplay.textContent = `Time: ${currentTime}`;
+    // reset the loop for another 30 seconds
+    setTimeout(displayCurrentTime, 30000);
+}
+
+displayCurrentTime();
+
+
+// just for testing and demo purpose
 const tempAssignments = [
     {
     className: "Math",
@@ -51,23 +75,59 @@ const tempAssignments = [
     },
 ];
 
+function filterAssignments(assignment, selectedClass, selectedDifficulty, selectedPriority) {
+    console.log(selectedClass)
+    console.log(assignment.className)
+    const classMatch = selectedClass === 'all' || assignment.className === selectedClass;
+    const difficultyMatch = selectedDifficulty === 'all' || assignment.difficulty === selectedDifficulty;
+    const priorityMatch = selectedPriority === 'all' || assignment.priority === selectedPriority;
 
-const tasksList = document.querySelector('.tasks ul');
-tasksList.innerHTML = '';
-tempAssignments.forEach((assignment) => {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `
-    <strong>Class:</strong> ${assignment.className}<br>
-    <strong>Project Type:</strong> ${assignment.projectType}<br>
-    <strong>Difficulty:</strong> ${assignment.difficulty}<br>
-    <strong>Priority:</strong> ${assignment.priority}<br>
-    <strong>Description:</strong> ${assignment.description}<br>
-    <strong>Assignment Name:</strong> ${assignment.assignmentName}<br>
-    <strong>Due Date:</strong> ${assignment.dueDate}<br>
-    <hr>
-    `;
+    console.log(classMatch &&  difficultyMatch && priorityMatch)
+    return classMatch && difficultyMatch && priorityMatch;
 
-    // Append the created list item to the tasksList
-    tasksList.appendChild(listItem);
+}
+
+
+function displayFilteredAssignments(selectedClass, selectedDifficulty, selectedPriority) {
+    const tasksList = document.querySelector('.tasks ul');
+    tasksList.innerHTML = '';
+
+    tempAssignments.forEach((assignment) => {
+        if (filterAssignments(assignment, selectedClass, selectedDifficulty, selectedPriority)) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <strong>Class:</strong> ${assignment.className}<br>
+            <strong>Project Type:</strong> ${assignment.projectType}<br>
+            <strong>Difficulty:</strong> ${assignment.difficulty}<br>
+            <strong>Priority:</strong> ${assignment.priority}<br>
+            <strong>Description:</strong> ${assignment.description}<br>
+            <strong>Assignment Name:</strong> ${assignment.assignmentName}<br>
+            <strong>Due Date:</strong> ${assignment.dueDate}<br>
+            <hr>`;
+        tasksList.appendChild(listItem);
+        }
+    });
+}
+
+const classFilter = document.getElementById('classFilter');
+const difficultyFilter = document.getElementById('difficultyFilter');
+const priorityFilter = document.getElementById('priorityFilter');
+
+classFilter.addEventListener('change', () => {
+    console.log("using class filter", classFilter.value.toString())
+    displayFilteredAssignments(classFilter.value.toString(), difficultyFilter.value.toString(), priorityFilter.value.toString());
 });
+
+difficultyFilter.addEventListener('change', () => {
+    displayFilteredAssignments(classFilter.value.toString(), difficultyFilter.value.toString(), priorityFilter.value.toString());
+});
+
+priorityFilter.addEventListener('change', () => {
+    displayFilteredAssignments(classFilter.value.toString(), difficultyFilter.value.toString(), priorityFilter.value.toString());
+});
+
+displayFilteredAssignments('all', 'all', 'all');
+
+
+
 
