e704057a7bde2c27401a8628d3cec15e40f93792
NathanAsayDong <nathan.dong@enzy.co>
2023-11-08 21:56:20 -0700

    updated to work with .json

--- index.js
+++ index.js
@@ -1,30 +1,15 @@
-const form = document.querySelector('#loginForm');
-console.log(form);
+const express = require('express');
+const app = express();
 
+// The service port. In production the frontend code is statically hosted by the service on the same port.
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log("Form Submit");
+// JSON body parsing using built-in middleware
+app.use(express.json());
 
-    // Get the username and password input values
-    const username = document.querySelector('#username').value;
-    const password = document.querySelector('#password').value;
+// Serve up the frontend static content hosting
+app.use(express.static('public'));
 
-    // logic to access login database and return true or false here
-
-
-    // if turn then redirect to home page
-    
-    window.location.href = "home.html";
-});
-
-function NateTestButton() {
-    console.log("NateTestButton")
-}
-
-// for now this is actually being used as a test button
-function forgotPassword() {
-    console.log("forgotPassword")
-
-    // implement password recent functionality here
-}
+// Router for service endpoints
+const apiRouter = express.Router();
+app.use(`/api`, apiRouter);
---
+++ public/index.js
@@ -0,0 +1,33 @@
+import { testValue } from "./firebaseAuth.service";
+console.log(testValue);
+
+const form = document.querySelector('#loginForm');
+console.log(form);
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    console.log("Form Submit");
+
+    // Get the username and password input values
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+
+    // logic to access login database and return true or false here
+
+
+    // if turn then redirect to home page
+    
+    window.location.href = "home.html";
+});
+
+function NateTestButton() {
+    console.log("NateTestButton")
+}
+
+// for now this is actually being used as a test button
+function forgotPassword() {
+    console.log("forgotPassword")
+
+    // implement password recent functionality here
+}
