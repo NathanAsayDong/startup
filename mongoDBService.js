a56b6369ef1f76cbf8b4332ced314043bda0a6db
NathanAsayDong <nathan.dong@enzy.co>
2023-11-18 14:04:28 -0700

    added simon login functions

--- mongoDBService.js
+++ mongoDBService.js
@@ -1,5 +1,7 @@
 const { MongoClient } = require('mongodb');
 const config = require('./dbConfig.json');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
